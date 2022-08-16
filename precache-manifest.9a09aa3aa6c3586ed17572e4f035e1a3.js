fc774c720c4a92ff23eabf9bd865fdd148806b54
Hasan-446 <hasanmahmudul020@gmail.com>
2022-08-17 01:14:41 +0600

    Updates

---
+++ precache-manifest.9a09aa3aa6c3586ed17572e4f035e1a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "57f3c39827594b394ff5",
+    "url": "/react.js-note-app/static/css/main.b692ec3d.chunk.css"
+  },
+  {
+    "revision": "57f3c39827594b394ff5",
+    "url": "/react.js-note-app/static/js/main.57f3c398.chunk.js"
+  },
+  {
+    "revision": "325ca37fb911515c36cd",
+    "url": "/react.js-note-app/static/js/1.325ca37f.chunk.js"
+  },
+  {
+    "revision": "78d3d6a1e1a4cd545455",
+    "url": "/react.js-note-app/static/css/2.e35607d6.chunk.css"
+  },
+  {
+    "revision": "78d3d6a1e1a4cd545455",
+    "url": "/react.js-note-app/static/js/2.78d3d6a1.chunk.js"
+  },
+  {
+    "revision": "4c16b15b37872ef988fc",
+    "url": "/react.js-note-app/static/js/runtime~main.4c16b15b.js"
+  },
+  {
+    "revision": "32ea9dab41a8f9a600d2caa24c8a65f0",
+    "url": "/react.js-note-app/index.html"
+  }
+];
